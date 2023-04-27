55ef03279a3303a9d046f6c3e08ef06e2b9d6129
farid-itpath <farid.itpath@gmail.com>
2023-04-27 14:09:01 +0530

    back to normal

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import Layout from "./layouts/AppLayout";
-import Profile from "./containers/Profile";
-import Home from "./containers/Home";
-import Product from "./containers/Product";
-import Cart from "./containers/Cart";
-import Login from "./containers/Login";
-import SignUp from "./containers/SignUp";
-import Error404 from "./containers/Error404"
+import { Error404, Login, SignUp, Home, Profile, Product, Cart } from "../containers";
+import Layout from "../layouts/AppLayout";
 
 export default function AppRoutes(){
     return (
