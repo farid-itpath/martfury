f65b7f4a8ef64e89ca4aa4b09f5977c5c68937af
farid-itpath <farid.itpath@gmail.com>
2023-04-27 13:41:37 +0530

    back to normal

--- src/Containers/index.js
+++ src/Containers/index.js
@@ -1,18 +1,18 @@
-// import Error404 from "./Error404";
-// import Login from "./Login";
-// import SignUp from "./SignUp";
-// import Home from "./Home";
-// import Profile from "./Profile";
-// import Product from "./Product";
-// import Cart from "./Cart";
+import Error404 from "./Error404";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import Home from "./Home";
+import Profile from "./Profile";
+import Product from "./Product";
+import Cart from "./Cart";
 
-// export {
-//     Error404,
-//     Login,
-//     SignUp,
-//     Home,
-//     Profile,
-//     Product,
-//     Cart,
+export {
+    Error404,
+    Login,
+    SignUp,
+    Home,
+    Profile,
+    Product,
+    Cart,
     
-// }
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-// import { Error404, Login, SignUp, Home, Profile, Product, Cart } from "./../containers";
-import Home from "../containers/Home";
-import Profile from "../containers/Profile"
-import Product from "../containers/Product"
-import Cart from "../containers/Cart"
-
-import Login from "../containers/Login"
-import SignUp from "../containers/SignUp"
-
-import Error404 from "../containers/Error404"
-
+import { Error404, Login, SignUp, Home, Profile, Product, Cart } from "../containers";
 import Layout from "../layouts/AppLayout";
 
 export default function AppRoutes(){
