61403143e6927616124cee441eb3f56e64780f12
farid-itpath <farid.itpath@gmail.com>
2023-04-27 13:37:54 +0530

    route file edited

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import { Error404, Login, SignUp, Home, Profile, Product, Cart } from "./../containers";
+// import { Error404, Login, SignUp, Home, Profile, Product, Cart } from "./../containers";
+import Home from "../containers/Home";
+import Profile from "../containers/Profile"
+import Product from "../containers/Product"
+import Cart from "../containers/Cart"
+
+import Login from "../containers/Login"
+import SignUp from "../containers/SignUp"
+
+import Error404 from "../containers/Error404"
+
 import Layout from "../layouts/AppLayout";
 
 export default function AppRoutes(){
